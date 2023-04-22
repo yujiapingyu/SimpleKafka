869377b4699aa78a82ab1c94c49efb505a26ff54
俞加平 <2507490921@qq.com>
2023-04-22 18:30:40 +0800

    reload

--- src/python/consumer/base_consumer.py
+++ src/python/consumer/base_consumer.py
@@ -2,7 +2,7 @@ import threading
 from confluent_kafka import Consumer, KafkaError
 
 class BaseKafkaConsumer(threading.Thread):
-    def __init__(self, conf, process_message_func, stop_event):
+    def __init__(self, conf, process_message_func, stop_event, start_func=None, end_func=None):
         threading.Thread.__init__(self)
         self.conf = conf
         self.topic = self.conf.pop('topic')
@@ -10,9 +10,14 @@ class BaseKafkaConsumer(threading.Thread):
         self.consumer = Consumer(self.conf)
         self.consumer.subscribe([self.topic])
         self.stop_event = stop_event
+        self.start_func = start_func
+        self.end_func = end_func
 
     def run(self):
         try:
+            if self.start_func is not None:
+                self.start_func()
+                
             while not self.stop_event.is_set():
                 msg = self.consumer.poll(1.0)
                 if msg is None:
@@ -21,6 +26,10 @@ class BaseKafkaConsumer(threading.Thread):
                     print('Consumer error: {}'.format(msg.error()))
                 else:
                     self.process_message(msg.value().decode('utf-8'))
+                    
+            if self.end_func is not None:
+                self.end_func()
+                
             print('Consumer [{}] stopped.'.format(self.__class__.__name__))
         except KeyboardInterrupt:
             print('KeyboardInterrupt...')
--- src/python/consumer/redis_consumer.py
+++ src/python/consumer/redis_consumer.py
@@ -1,14 +1,33 @@
 import json
 import redis
+import pickle
+import os
+
 from .base_consumer import BaseKafkaConsumer
 from config import ConsumerConfig
 
 class RedisConsumer(BaseKafkaConsumer):
     def __init__(self, conf, stop_event):
-        super().__init__(conf, self.process_message, stop_event)
+        super().__init__(conf, self.process_message, stop_event, self.read_buffer_from_file, self.save_buffer_to_file)
         self.redis = redis.Redis(host='localhost', port=6379, db=0)
         self.buffer = []
+        self.pickle_file = 'buffer.pickle'
         
+    def save_buffer_to_file(self):
+        with open(self.pickle_file, 'wb') as f:
+            pickle.dump(self.buffer, f)
+        print('saved buffer to file.[{}]'.format(self.pickle_file))
+        
+    def read_buffer_from_file(self):
+        if not os.path.exists(self.pickle_file):
+            print('pickle file not exists.[{}]'.format(self.pickle_file))
+            return
+        with open(self.pickle_file, "rb") as file:
+            self.buffer = pickle.load(file)
+        print('read buffer from file.[{}]'.format(self.pickle_file))
+        # 删除文件
+        os.remove(self.pickle_file)
+    
     def flush(self):
         print('Flushing buffer: {}'.format(self.buffer))
         for obj in self.buffer:
