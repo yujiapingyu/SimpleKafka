d36419299d7b96d58e0013fb2cf8c7058b8ca3fa
yujiaping <2507490921@qq.com>
2023-04-24 12:00:40 +0000

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-./tool/bazel build :all
+# 判断平台是arm的还是amd的
+if [ $(uname -m) = "x86_64" ]; then
+    echo "Ploatform: amd64"
+    ./tool/bazel_amd build :all
+else
+    echo "Ploatform: arm"
+    ./tool/bazel_arm build :all
+fi
 
 cp -f ./bazel-bin/main_work ./main_work
